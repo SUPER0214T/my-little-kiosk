9e41cf8cdc51cfe017e75e78e9ab326c702c4eb6
JunYoung <twinjyjh2@gmail.com>
2023-09-16 23:40:48 +0900

    Revert "[fix] 커밋 테스트"
    This reverts commit 0a48d63e3928f110bd2a75edbbf1be6c1bed6aba.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Order from './components/pages/Order/Order';
 import { ROUTES } from './constants/routes';
-import { customAxios } from './services/axios';
 
 function App() {
-  useEffect(() => {
-    customAxios.get('/master').then((res) => console.log(res.data));
-  }, []);
   return (
     <div>
       <BrowserRouter>
