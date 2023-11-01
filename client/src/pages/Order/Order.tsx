62e62fcb3a2a1e4a104f183f20ce4ee33f499c55
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-01 22:35:15 +0900

    [feat] 임시 order 페이지 생성 및 스타일 구현

--- client/src/pages/Order/Order.tsx
+++ client/src/pages/Order/Order.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import './OrderItem.css';
+import { ItemData } from '../../types/master';
+import { useAppSelector } from '../../hooks/useStore';
 
 function Order() {
-  return <div>Order페이지</div>;
+  return (
+    <div>
+      <OrderList />
+    </div>
+  );
 }
 
 export default Order;
+
+// OrderList
+function OrderList() {
+  // const [itemData, setItemData] = useState<ItemData[]>([]);
+  // const getMasterItemData = () => {
+  //   customAxios.get('/master').then((res) => setItemData(res.data));
+  // };
+
+  // useEffect(() => {
+  //   getMasterItemData();
+  // }, []);
+  const { itemData } = useAppSelector((state) => state.masterReducer);
+
+  return (
+    <div className="order-list">
+      {itemData ? itemData.map((item) => <OrderItem itemData={item} />) : <div>존재하지 않습니다.</div>}
+    </div>
+  );
+}
+
+// OrderItem
+interface OrderItemProps {
+  itemData: ItemData;
+}
+
+function OrderItem({ itemData }: OrderItemProps) {
+  return (
+    <div className="order-item">
+      <div className="image-wrapper">
+        <img src={itemData.IMG_URL} />
+      </div>
+      <div className="name">{itemData.ITEM_NM}</div>
+      <div className="price">{itemData.ITEM_PRIC}원</div>
+    </div>
+  );
+}
