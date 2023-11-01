ba56181dd6c9742a9f9bc026e14d0184ec8aa6ba
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-01 22:37:08 +0900

    [fix] 테스트 실행시 theme 찾을 수 없는 문제 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,22 @@ import { Route, Routes } from 'react-router-dom';
 import Order from './pages/Order/Order';
 import { ROUTES } from './constants/routes';
 import { reset } from './styles/globalStyle';
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { Wrapper } from './App.styles';
 import Home from './pages/Home/Home';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <Wrapper>
-      <Global styles={reset} />
-      <Routes>
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.ORDER} element={<Order />} />
-      </Routes>
-    </Wrapper>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Global styles={reset} />
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.ORDER} element={<Order />} />
+        </Routes>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -17,11 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
