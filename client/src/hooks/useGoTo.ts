07294c92f85f1dd01e13325c03407c545d9fdccf
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-04 20:43:45 +0900

    [feat] useNavigate 중복을 줄이기 위한 useGoTo 함수 생성

---
+++ client/src/hooks/useGoTo.ts
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../constants/routes';
+
+export const useGoTo = () => {
+  const navigate = useNavigate();
+
+  const goTo = (url: string) => navigate(url);
+
+  const goToHome = () => goTo(ROUTES.HOME);
+  const goToOrder = () => goTo(ROUTES.ORDER);
+  const goToOrderDetail = () => goTo(ROUTES.ORDER_DETAIL);
+  const goToCheckout = () => goTo(ROUTES.CHECKOUT);
+  const goToPayment = () => goTo(ROUTES.PAYMENT);
+  const goToConfirmation = () => goTo(ROUTES.CONFIRMATION);
+
+  return { goTo, goToHome, goToOrder, goToOrderDetail, goToCheckout, goToPayment, goToConfirmation };
+};
