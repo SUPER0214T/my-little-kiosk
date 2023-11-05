28dadbfd0590980fc9fbeed2ffde164519f36a00
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-05 22:15:57 +0900

    [test] redux store가 이전 테스트의 영향을 받지 않도록 mockStore 구현

--- client/src/utils/testUtils.tsx
+++ client/src/utils/testUtils.tsx
@@ -2,9 +2,25 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
-import { store } from '../redux/store';
 import App from '../App';
 import React from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from '../redux/rootReducer';
+import logger from 'redux-logger';
+
+// redux
+export const createStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    middleware:
+      process.env.NODE_ENV === 'development'
+        ? (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+              serializableCheck: false,
+            }).concat(logger)
+        : undefined,
+    devTools: process.env.NODE_ENV === 'development',
+  });
 
 // flow
 export const goToOrderFlow = async () => {
@@ -16,20 +32,31 @@ export const goToOrderFlow = async () => {
 };
 
 // render
+/**
+ * @desc mockStore를 사용하여 이전 테스트의 store에 접근하는 것을 방지
+ */
 export const renderSimplify = (initialEntries: string[] = ['/']) => {
-  return render(
+  const mockStore = createStore();
+  const renderResult = render(
     <MemoryRouter initialEntries={initialEntries}>
-      <Provider store={store}>
+      <Provider store={mockStore}>
         <App />
       </Provider>
     </MemoryRouter>,
   );
+
+  return {
+    mockStore,
+    renderResult,
+  };
 };
 
 const insideRender = (initialEntries: string[] = ['/']) => {
+  const mockStore = createStore();
+
   return (
     <MemoryRouter initialEntries={initialEntries}>
-      <Provider store={store}>
+      <Provider store={mockStore}>
         <App />
       </Provider>
     </MemoryRouter>
