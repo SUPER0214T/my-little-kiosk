b73df04f134b0b8f2ee3443da42b4c08b9d4e9a7
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-05 22:19:00 +0900

    [test] Header.spec.tsx의 render 함수를 renderSimply 함수로 변경

--- client/src/components/common/Header/__tests__/Header.spec.tsx
+++ client/src/components/common/Header/__tests__/Header.spec.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { store } from '../../../../redux/store';
-import App from '../../../../App';
+import { renderSimplify } from '../../../../utils/testUtils';
 
 describe('Header 컴포넌트', () => {
   it('Home 버튼이 존재한다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/order']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
+    renderSimplify(['/order']);
 
     const homeBtn = await screen.findByText('Home');
     expect(homeBtn).toBeInTheDocument();
   });
 
   it('Home 버튼을 클릭하면 Home으로 이동한다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/order']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
+    renderSimplify(['/order']);
 
     const homeBtn = await screen.findByText('Home');
     userEvent.click(homeBtn);
