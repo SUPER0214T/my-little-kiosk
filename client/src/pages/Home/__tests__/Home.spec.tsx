bb1c63844f56bd29aa4c894a8b95c1b6dfee2e98
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-06 22:43:56 +0900

    [test] Home으로 이동시 basketList 초기화하는 기능 테스트

---
+++ client/src/pages/Home/__tests__/Home.spec.tsx
@@ -0,0 +1,32 @@
+import { goToOrderFlow, renderSimplify } from '../../../utils/testUtils';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Home', () => {
+  //@todo 아래 테스트 작성
+  it('Home에서는 장바구니가 비어있다.', async () => {
+    const { mockStore } = renderSimplify();
+    await goToOrderFlow();
+
+    const orderItem = await screen.findByText('티셔츠 237');
+    userEvent.click(orderItem);
+
+    const confirmBtn = await screen.findByText('구매하기');
+    userEvent.click(confirmBtn);
+
+    const checkoutBtn = await screen.findByText('결제하기');
+    userEvent.click(checkoutBtn);
+
+    const isCheckoutPage = await screen.findByText('주문 확인');
+    expect(isCheckoutPage).toBeInTheDocument();
+
+    const homeBtn = await screen.findByText('Home');
+    userEvent.click(homeBtn);
+
+    const masterDownBtn = await screen.findByText('마스터 수신');
+    expect(masterDownBtn).toBeInTheDocument();
+
+    const { basketList } = mockStore.getState().basketReducer;
+    expect(basketList).toHaveLength(0);
+  });
+});
