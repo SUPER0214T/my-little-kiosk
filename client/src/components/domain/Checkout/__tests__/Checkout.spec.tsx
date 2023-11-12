8c182b7892484d007aa616a2bc6258de14d8e0c2
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-12 23:38:10 +0900

    [test] checkout 페이지에서 장바구니 상품이 여러 개 존재할 때 첫 번째 상품의 개수를 증가시킨 후 총 금액 검증 테스트 추가

--- client/src/components/domain/Checkout/__tests__/Checkout.spec.tsx
+++ client/src/components/domain/Checkout/__tests__/Checkout.spec.tsx
@@ -1,5 +1,5 @@
 import { goToOrderFlow, renderSimplify } from '../../../../utils/testUtils';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('Checkout', () => {
@@ -73,30 +73,30 @@ describe('Checkout', () => {
   });
 
   /*@todo 아래 고쳐야 함.*/
-  // it('checkout 페이지에서 counter가 여러 개 존재해도 구매한 0번째의 개수를 1개 늘리면 가격이 2배가 된다.', async () => {
-  //   renderSimplify();
-  //   await goToOrderFlow();
-  //
-  //   const orderItem = await screen.findByText('티셔츠 237');
-  //   userEvent.click(orderItem);
-  //
-  //   const confirmBtn = await screen.findByText('구매하기');
-  //   userEvent.click(confirmBtn);
-  //
-  //   const orderItem03 = await screen.findByText('원피스 196');
-  //   userEvent.click(orderItem03);
-  //
-  //   const confirmBtn03 = await screen.findByText('구매하기');
-  //   userEvent.click(confirmBtn03);
-  //
-  //   const checkoutBtn = await screen.findByText('결제하기');
-  //   userEvent.click(checkoutBtn);
-  //
-  //   const plusBtn = await screen.findAllByText('+');
-  //   expect(plusBtn[0]).toBeInTheDocument();
-  //   userEvent.click(plusBtn[0]);
-  //
-  //   const totalBasketAmount = await screen.findByText('총 주문 금액: 7683원');
-  //   expect(totalBasketAmount).toBeInTheDocument();
-  // });
+  it('checkout 페이지에서 counter가 여러 개 존재해도 구매한 0번째의 개수를 1개 늘리면 가격이 2배가 된다.', async () => {
+    renderSimplify();
+    await goToOrderFlow();
+
+    const orderItem = await screen.findByText('티셔츠 237');
+    userEvent.click(orderItem);
+
+    const confirmBtn = await screen.findByText('구매하기');
+    userEvent.click(confirmBtn);
+
+    const orderItem03 = await screen.findByText('원피스 196');
+    userEvent.click(orderItem03);
+
+    const confirmBtn03 = await screen.findByText('구매하기');
+    userEvent.click(confirmBtn03);
+
+    const checkoutBtn = await screen.findByText('결제하기');
+    userEvent.click(checkoutBtn);
+
+    const plusBtn = await screen.findAllByText('+');
+    expect(plusBtn[0]).toBeInTheDocument();
+    userEvent.click(plusBtn[0]);
+
+    const totalBasketAmount = await screen.findByText('총 주문 금액: 7682원');
+    expect(totalBasketAmount).toBeInTheDocument();
+  });
 });
