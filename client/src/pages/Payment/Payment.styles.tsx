5d52e65e6549e9120bab20d01d42d154aa4a146d
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-14 23:06:38 +0900

    [feat] payment의 styled component 분리

---
+++ client/src/pages/Payment/Payment.styles.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export const Layout = styled.div``;
+
+export const Title = styled.div`
+  font-size: 2rem;
+  text-align: center;
+  margin: 2rem;
+`;
+
+export const PaymentSelect = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PaymentList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const PaymentCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 1rem;
+  font-size: 2rem;
+  cursor: pointer;
+  flex-wrap: wrap;
+  margin: 1rem;
+`;
+
+export const TotalPaymentAmount = styled.div`
+  font-size: 2rem;
+  text-align: center;
+  color: red;
+  margin-top: 1rem;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Btn = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+  padding: 1rem;
+  border: 1px solid black;
+  border-radius: 2rem;
+  min-width: 150px;
+  cursor: pointer;
+`;
+
+export const CancelBtn = styled.div`
+  ${Btn};
+`;
--- client/src/pages/Payment/Payment.tsx
+++ client/src/pages/Payment/Payment.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import useBasket from '../../hooks/useBasket';
 import { combineBasketInfo } from '../Checkout/Checkout';
 import { useAppSelector } from '../../hooks/useStore';
-import { css } from '@emotion/react';
 import Header from '../../components/common/Header/Header';
 import { useGoTo } from '../../hooks/useGoTo';
+import * as S from './Payment.styles';
 
 const PAYMENT_TYPE = ['credit', 'naver', 'kakao'];
 
@@ -21,84 +20,22 @@ function Payment() {
   };
 
   return (
-    <Layout>
+    <S.Layout>
       <Header />
-      <Title>결제 방법을 선택해주세요.</Title>
-      <PaymentSelect>
-        <PaymentList>
+      <S.Title>결제 방법을 선택해주세요.</S.Title>
+      <S.PaymentSelect>
+        <S.PaymentList>
           {PAYMENT_TYPE.map((el) => (
-            <PaymentCard>{el}</PaymentCard>
+            <S.PaymentCard>{el}</S.PaymentCard>
           ))}
-        </PaymentList>
-      </PaymentSelect>
-      <TotalPaymentAmount>총 결제금액: {totalBasketAmount}</TotalPaymentAmount>
-      <Footer>
-        <CancelBtn onClick={handleCancelClick}>취소</CancelBtn>
-      </Footer>
-    </Layout>
+        </S.PaymentList>
+      </S.PaymentSelect>
+      <S.TotalPaymentAmount>총 결제금액: {totalBasketAmount}</S.TotalPaymentAmount>
+      <S.Footer>
+        <S.CancelBtn onClick={handleCancelClick}>취소</S.CancelBtn>
+      </S.Footer>
+    </S.Layout>
   );
 }
 
 export default Payment;
-
-const Layout = styled.div``;
-
-const Title = styled.div`
-  font-size: 2rem;
-  text-align: center;
-  margin: 2rem;
-`;
-
-const PaymentSelect = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const PaymentList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const PaymentCard = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-  height: 100px;
-  border: 1px solid black;
-  border-radius: 1rem;
-  font-size: 2rem;
-  cursor: pointer;
-  flex-wrap: wrap;
-  margin: 1rem;
-`;
-
-const TotalPaymentAmount = styled.div`
-  font-size: 2rem;
-  text-align: center;
-  color: red;
-  margin-top: 1rem;
-`;
-
-const Footer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Btn = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 2rem;
-  padding: 1rem;
-  border: 1px solid black;
-  border-radius: 2rem;
-  min-width: 150px;
-  cursor: pointer;
-`;
-
-const CancelBtn = styled.div`
-  ${Btn};
-`;
