2b18dacbd34011ee8df7523c83d6e18e5744d8d6
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-18 17:42:51 +0900

    [test] home 페이지 테스트에 유틸 함수 적용

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import App from './App';
-import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import configureStore from 'redux-mock-store';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import MockAdapter from 'axios-mock-adapter';
 import { customAxios } from './services/axios';
 import { API_LIST } from './constants/apiList';
 import { mockMasterItemData } from './mocks/handlers';
+import { goToOrderFlow, renderSimplify } from './utils/testUtils';
 /**
  * 근데 아래의 것들을 굳이 테스트해야 할까?
  * 그래도 일단 제대로 호출 되었는지는 알아야 하니까 체크를 해보자. 공부도 할 겸
@@ -35,45 +30,22 @@ describe('setup', () => {
   // });
 
   it('메인 페이지에는 주문하기 버튼이 존재한다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
+    renderSimplify();
 
     const orderButton = await screen.findByText('주문하기');
     expect(orderButton).toBeInTheDocument();
   });
 
   it('메인 페이지에는 마스터 수신 버튼이 존재한다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
+    renderSimplify();
 
     const masterDownButton = await screen.findByText('마스터 수신');
     expect(masterDownButton).toBeInTheDocument();
   });
 
   it('order 페이지에는 `티셔츠 237`이 존재한다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
-
-    const masterDownButton = await screen.findByText('마스터 수신');
-    userEvent.click(masterDownButton);
-
-    const orderBtn = await screen.findByText('주문하기');
-    userEvent.click(orderBtn);
+    renderSimplify();
+    await goToOrderFlow();
 
     const orderText = await screen.findByText('티셔츠 237');
     expect(orderText).toBeInTheDocument();
@@ -95,17 +67,11 @@ describe('setup', () => {
   // });
 
   it('master 수신 버튼을 클릭하면 store에 데이터가 저장된다.', async () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MemoryRouter>,
-    );
+    const { mockStore } = renderSimplify();
 
     const masterDownButton = await screen.findByText('마스터 수신');
     userEvent.click(masterDownButton);
 
-    await waitFor(() => expect(store.getState().masterReducer.itemData).toHaveLength(4));
+    await waitFor(() => expect(mockStore.getState().masterReducer.itemData).toHaveLength(4));
   });
 });
