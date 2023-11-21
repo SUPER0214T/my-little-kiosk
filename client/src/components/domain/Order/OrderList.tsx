971225f2fdcd4efd2bceb6ea9c4687e9b211d4ba
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-21 21:35:10 +0900

    [feat] OrderList react-query로 캐싱된 값 가져오도록 적용

--- client/src/components/domain/Order/OrderList.tsx
+++ client/src/components/domain/Order/OrderList.tsx
@@ -1,21 +1,22 @@
 import { useAppSelector } from '../../../hooks/useStore';
-import React from 'react';
+import React, { useEffect } from 'react';
 import OrderItem from './OrderItem';
+import { QueryCache, useQuery, useQueryClient } from 'react-query';
+import { getMasterAll } from '../../../services/master';
+import { ItemData } from '../../../types/master';
+
+const useGetQueryData: <ResType>(key: string[]) => ResType | undefined = (key) => {
+  const queryClient = useQueryClient();
+
+  return queryClient.getQueryData(key);
+};
 
 function OrderList() {
-  // const [itemData, setItemData] = useState<ItemData[]>([]);
-  // const getMasterItemData = () => {
-  //   customAxios.get('/master').then((res) => setItemData(res.data));
-  // };
-
-  // useEffect(() => {
-  //   getMasterItemData();
-  // }, []);
-  const { itemData } = useAppSelector((state) => state.masterReducer);
+  const itemData = useGetQueryData<{ data: ItemData[] }>(['home', 'getMasterAll']);
 
   return (
     <div className="order-list" data-testid={'order-list'}>
-      {itemData ? itemData.map((item) => <OrderItem itemData={item} />) : <div>존재하지 않습니다.</div>}
+      {itemData ? itemData.data.map((item) => <OrderItem itemData={item} />) : <div>존재하지 않습니다.</div>}
     </div>
   );
 }
