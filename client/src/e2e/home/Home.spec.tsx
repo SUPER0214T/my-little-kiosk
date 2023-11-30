737f35563e11a71e938351ecfbc3c54ce0185fd4
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-30 23:35:40 +0900

    [test] Home.spec.tsx의 테스트를 유틸 함수를 사용하여 테스트하도록 리팩터링

--- client/src/e2e/home/Home.spec.tsx
+++ client/src/e2e/home/Home.spec.tsx
@@ -1,20 +1,13 @@
-import { goToOrderFlow, renderSimplify } from '../../utils/testUtils';
+import { buyOneItem, goToCheckoutFlow, renderSimplify } from '../../utils/testUtils';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('Home', () => {
   it('Home에서는 장바구니가 비어있다.', async () => {
     const { mockStore } = renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+    });
 
     const isCheckoutPage = await screen.findByText('주문 확인');
     expect(isCheckoutPage).toBeInTheDocument();
