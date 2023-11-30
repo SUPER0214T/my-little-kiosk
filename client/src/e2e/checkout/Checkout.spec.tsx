d71d5d694f875c99e7661d07e50703b25b0a6529
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-30 23:29:12 +0900

    [test] Checkout.spec.tsx의 테스트를 유틸 함수를 사용하여 테스트하도록 리팩터링

--- client/src/e2e/checkout/Checkout.spec.tsx
+++ client/src/e2e/checkout/Checkout.spec.tsx
@@ -1,20 +1,13 @@
-import { goToOrderFlow, renderSimplify } from '../../utils/testUtils';
-import { act, screen } from '@testing-library/react';
+import { buyOneItem, goToCheckoutFlow, renderSimplify } from '../../utils/testUtils';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('Checkout', () => {
   it('티셔츠 237 상품을 구매하면 1542원이 표시된다.', async () => {
     renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+    });
 
     const totalBasketAmount = await screen.findByText('총 주문 금액: 1542원');
     expect(totalBasketAmount).toBeInTheDocument();
@@ -22,28 +15,11 @@ describe('Checkout', () => {
 
   it('세 개를 구매하면 9,908원이 표시된다.', async () => {
     renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const orderItem02 = await screen.findByText('바지 495');
-    userEvent.click(orderItem02);
-
-    const confirmBtn02 = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn02);
-
-    const orderItem03 = await screen.findByText('원피스 196');
-    userEvent.click(orderItem03);
-
-    const confirmBtn03 = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn03);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+      await buyOneItem('바지 495');
+      await buyOneItem('원피스 196');
+    });
 
     const totalBasketAmount = await screen.findByText('총 주문 금액: 9908원');
     expect(totalBasketAmount).toBeInTheDocument();
@@ -51,16 +27,9 @@ describe('Checkout', () => {
 
   it('checkout 페이지에서 구매한 0번째의 개수를 1개 늘리면 가격이 2배가 된다.', async () => {
     renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+    });
 
     const totalBasketAmount = await screen.findByText('총 주문 금액: 1542원');
     expect(totalBasketAmount).toBeInTheDocument();
@@ -72,25 +41,12 @@ describe('Checkout', () => {
     expect(totalBasketAmountAfterBtnClick).toBeInTheDocument();
   });
 
-  /*@todo 아래 고쳐야 함.*/
   it('checkout 페이지에서 counter가 여러 개 존재해도 구매한 0번째의 개수를 1개 늘리면 가격이 2배가 된다.', async () => {
     renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const orderItem03 = await screen.findByText('원피스 196');
-    userEvent.click(orderItem03);
-
-    const confirmBtn03 = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn03);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+      await buyOneItem('원피스 196');
+    });
 
     const plusBtn = await screen.findAllByText('+');
     expect(plusBtn[0]).toBeInTheDocument();
