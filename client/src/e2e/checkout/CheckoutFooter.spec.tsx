13c12bbb363c6c93e167f81f30efbcd9196b3dee
JunYoung Kim <twinjyjh2@gmail.com>
2023-11-30 23:34:24 +0900

    [test] CheckoutFooter.spec.tsx의 테스트를 유틸 함수를 사용하여 테스트하도록 리팩터링

--- client/src/e2e/checkout/CheckoutFooter.spec.tsx
+++ client/src/e2e/checkout/CheckoutFooter.spec.tsx
@@ -1,20 +1,13 @@
-import { buyOneItem, goToOrderFlow, goToPaymentFlow, renderSimplify } from '../../utils/testUtils';
+import { buyOneItem, goToCheckoutFlow, goToPaymentFlow, renderSimplify } from '../../utils/testUtils';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('CheckoutFooter', () => {
   it('이전을 클릭하면 Order로 이동한다.', async () => {
     renderSimplify();
-    await goToOrderFlow();
-
-    const orderItem = await screen.findByText('티셔츠 237');
-    userEvent.click(orderItem);
-
-    const confirmBtn = await screen.findByText('구매하기');
-    userEvent.click(confirmBtn);
-
-    const checkoutBtn = await screen.findByText('결제하기');
-    userEvent.click(checkoutBtn);
+    await goToCheckoutFlow(async () => {
+      await buyOneItem('티셔츠 237');
+    });
 
     const isCheckoutPage = await screen.findByText('주문 확인');
     expect(isCheckoutPage).toBeInTheDocument();
