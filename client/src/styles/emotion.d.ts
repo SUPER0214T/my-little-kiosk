8353b203be819164251a81aea58a627c6f9f82d0
JunYoung Kim <twinjyjh2@gmail.com>
2023-12-06 21:32:35 +0900

    [feat] theme 타입 정의, theme 색상 정의

--- client/src/styles/emotion.d.ts
+++ client/src/styles/emotion.d.ts
@@ -2,21 +2,10 @@ import '@emotion/react';
 
 declare module '@emotion/react' {
   export interface Theme {
-    light: {
-      border1: string;
-      border2: string;
-      border3: string;
-      border4: string;
-      primary1: string;
-      primary2: string;
-    };
-    dark: {
-      border1: string;
-      border2: string;
-      border3: string;
-      border4: string;
-      primary1: string;
-      primary2: string;
-    };
+    primaryLight: string;
+    primary: string;
+    primaryDark: string;
+    white: string;
+    black: string;
   }
 }
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -1,22 +1,11 @@
 import { Theme } from '@emotion/react';
 
 const theme: Theme = {
-  light: {
-    border1: '#343A40',
-    border2: '#ADB5BD',
-    border3: '#DEE2E6',
-    border4: '#F1F3F5',
-    primary1: '#12B886',
-    primary2: '#20C997',
-  },
-  dark: {
-    border1: '#343A40',
-    border2: '#ADB5BD',
-    border3: '#DEE2E6',
-    border4: '#F1F3F5',
-    primary1: '#12B886',
-    primary2: '#20C997',
-  },
+  primaryLight: '#FFE9D0',
+  primary: '#DB8A4D',
+  primaryDark: '#8D603F',
+  white: '#fff',
+  black: 'black',
 };
 
 export default theme;
