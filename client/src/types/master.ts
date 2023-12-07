a63d642de26f7065364b57090fa8c02f97a88b98
JunYoung Kim <twinjyjh2@gmail.com>
2023-12-07 23:20:43 +0900

    [feat] 프론트의 마스터 타입에 BADGE_IDX 추가

--- client/src/types/master.ts
+++ client/src/types/master.ts
@@ -3,6 +3,7 @@ export type ItemData = {
   ITEM_NM: string;
   ITEM_PRIC: number;
   IMG_URL: string;
+  BADGE_IDX: number | null;
 };
 
 export type MasterData = { ITEM_DATA: ItemData[]; BADGE_LIST: string[] };
