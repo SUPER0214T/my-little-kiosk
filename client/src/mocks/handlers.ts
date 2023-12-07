80ab43786792cdd6f64ca9247e76de435bc054c5
JunYoung Kim <twinjyjh2@gmail.com>
2023-12-07 23:00:18 +0900

    [feat] MSW의 마스터 수신 모킹에 BADGE_LIST 추가

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,31 +1,38 @@
 import { rest } from 'msw';
 
-export const mockMasterItemData = [
-  {
-    IMG_URL: 'https://picsum.photos/id/736/200/300',
-    ITEM_CD: '37',
-    ITEM_NM: '티셔츠 237',
-    ITEM_PRIC: 1542,
-  },
-  {
-    IMG_URL: 'https://picsum.photos/id/15/200/300',
-    ITEM_CD: '86',
-    ITEM_NM: '원피스 886',
-    ITEM_PRIC: 6364,
-  },
-  {
-    IMG_URL: 'https://picsum.photos/id/523/200/300',
-    ITEM_CD: '95',
-    ITEM_NM: '바지 495',
-    ITEM_PRIC: 3768,
-  },
-  {
-    IMG_URL: 'https://picsum.photos/id/492/200/300',
-    ITEM_CD: '96',
-    ITEM_NM: '원피스 196',
-    ITEM_PRIC: 4598,
-  },
-];
+export const mockMasterItemData = {
+  ITEM_DATA: [
+    {
+      IMG_URL: 'https://picsum.photos/id/736/200/300',
+      ITEM_CD: '37',
+      ITEM_NM: '티셔츠 237',
+      ITEM_PRIC: 1542,
+    },
+    {
+      IMG_URL: 'https://picsum.photos/id/15/200/300',
+      ITEM_CD: '86',
+      ITEM_NM: '원피스 886',
+      ITEM_PRIC: 6364,
+    },
+    {
+      IMG_URL: 'https://picsum.photos/id/523/200/300',
+      ITEM_CD: '95',
+      ITEM_NM: '바지 495',
+      ITEM_PRIC: 3768,
+    },
+    {
+      IMG_URL: 'https://picsum.photos/id/492/200/300',
+      ITEM_CD: '96',
+      ITEM_NM: '원피스 196',
+      ITEM_PRIC: 4598,
+    },
+  ],
+  BADGE_LIST: [
+    'https://picsum.photos/id/980/400/400',
+    'https://picsum.photos/id/204/400/400',
+    'https://picsum.photos/id/103/400/400',
+  ],
+};
 
 export const handlers = [
   rest.get('/api/master', (req, res, ctx) => {
