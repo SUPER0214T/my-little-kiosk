9f2a1da9a923f49252c6f0ffe13649cd93110e92
JunYoung Kim <twinjyjh2@gmail.com>
2023-12-07 23:21:47 +0900

    [feat] 마스터의 ITEM_DATA에 뱃지 정보가 추가됨에 따라 MSW의 핸들러에 BADGE_IDX 추가

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -7,24 +7,28 @@ export const mockMasterItemData = {
       ITEM_CD: '37',
       ITEM_NM: '티셔츠 237',
       ITEM_PRIC: 1542,
+      BADGE_IDX: 0,
     },
     {
       IMG_URL: 'https://picsum.photos/id/15/200/300',
       ITEM_CD: '86',
       ITEM_NM: '원피스 886',
       ITEM_PRIC: 6364,
+      BADGE_IDX: 1,
     },
     {
       IMG_URL: 'https://picsum.photos/id/523/200/300',
       ITEM_CD: '95',
       ITEM_NM: '바지 495',
       ITEM_PRIC: 3768,
+      BADGE_IDX: 2,
     },
     {
       IMG_URL: 'https://picsum.photos/id/492/200/300',
       ITEM_CD: '96',
       ITEM_NM: '원피스 196',
       ITEM_PRIC: 4598,
+      BADGE_IDX: null,
     },
   ],
   BADGE_LIST: [
